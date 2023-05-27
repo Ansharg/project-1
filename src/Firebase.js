ceb8a3cd0e4d084aefdd061668e2e951118c7953
Ansharg <anshgarg94161@gmail.com>
2023-05-27 23:49:10 +0530

    before using firebase for the project

---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHjCnFjbxyJuAOmW15MUCeVQsJVG2fSk0",
+  authDomain: "project-1-4b168.firebaseapp.com",
+  projectId: "project-1-4b168",
+  storageBucket: "project-1-4b168.appspot.com",
+  messagingSenderId: "920124451056",
+  appId: "1:920124451056:web:d7750b86e959017e007a35"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
