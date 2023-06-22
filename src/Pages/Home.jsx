f7f99c857ed980a86aad31178055503d1d57a12b
Ansharg <anshgarg94161@gmail.com>
2023-06-22 12:27:32 +0530

    here we completed the sign in page of our website

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Cards from '../Components/Cards'
 
 export default function Home() {
   return (
--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import {AiFillEyeInvisible,AiFillEye} from 'react-icons/ai'
-import {Link} from 'react-router-dom'
+import {Link , useNavigate} from 'react-router-dom'
 import OAuth from '../Components/OAuth';
+import { signInWithEmailAndPassword,getAuth } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 export default function SignIn() {
   const [showPassword,setShowPassword] = useState(false);
+  const navigate = useNavigate();
   const [formData,setFormData] = useState({
     email: "",
     password: "",
@@ -17,6 +20,20 @@ export default function SignIn() {
       [e.target.id]: e.target.value
     }));
   }
+  async function onSubmit(e)
+  {
+    e.preventDefault()
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      if(userCredential.user)
+      {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Bad user credentials");
+    }
+  }
   return (
     <>
     <section>
@@ -26,7 +43,7 @@ export default function SignIn() {
           <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1673&q=80" alt="key" className="w-full rounded-2xl"/>
         </div>
         <div className="md:w-[67%] lg:w-[40%] lg:ml-20 relative top-[-0.5rem]">
-          <form action="" className='flex flex-col space-y-6'>
+          <form action="" className='flex flex-col space-y-6' onSubmit={onSubmit}>
             <input type="email" id='email'  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" placeholder='Email Address' value={email} onChange={onChange}/>
             <div className='relative '>
             <input type={showPassword ? "text":"password"} id='password'  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" placeholder='Password' value={password} onChange={onChange}/>{showPassword ? (<AiFillEyeInvisible className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=>setShowPassword((prevState)=>!prevState)} />) : (<AiFillEye className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=>setShowPassword((prevState)=>!prevState)} />) }
