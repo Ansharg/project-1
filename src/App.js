56587afba5b376a2c51cbf9c88496fa16f994419
Ansharg <anshgarg94161@gmail.com>
2023-06-23 01:31:27 +0530

    here we completed the private Route and make our own Hook important for learning

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Profile from "./Pages/Profile"
 import Header from "./Components/Header"
 import {ToastContainer} from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
+import PrivateRoute from "./Components/PrivateRoute"
 
 
 
@@ -18,6 +19,9 @@ function App() {
         <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/profile" element={<PrivateRoute />} >
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn/>}/>
           <Route path="/sign-up" element={<SignUp/>}/>
           <Route path="/forgot-password" element={<ForgotPassword/>}/>
